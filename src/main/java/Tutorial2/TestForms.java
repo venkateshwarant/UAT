2e338bc82d269c47a22131c31b4b4763d9fb6bea
Venkateshwaran Thamilselvan <venkat@mf0055.uni.lux>
2019-10-22 17:07:49 +0200

    new test cases added

---
+++ src/main/java/Tutorial2/TestForms.java
@@ -0,0 +1,67 @@
+package Tutorial2;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestForms {
+	private static WebDriver driver;
+	private static String firstName= "Venkateshwaran";
+	private static String lastName= "Thamilselvan";
+	
+	@BeforeTest
+	public static void configureDriver() {
+		System.setProperty("webdriver.chrome.driver" ,  "/Users/venkat/Downloads/chromedriver");
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+	@Test
+	public static void testFirstNameField() throws InterruptedException {
+		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/java/Tutorial2/forms.html");
+		Thread.sleep(1000);
+		driver.findElement(By.id("firstname")).sendKeys(firstName);
+		Thread.sleep(1000);
+		String fNameInForm = driver.findElement(By.id("firstname")).getAttribute("value");
+		Assert.assertEquals(firstName, fNameInForm);
+	}
+	
+	@Test //This should fail as we are asserting lastname field value with the firstname value
+	public static void testLastNameField() throws InterruptedException {
+		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/java/Tutorial2/forms.html");
+		Thread.sleep(1000);
+		driver.findElement(By.id("lastname")).sendKeys(lastName);
+		Thread.sleep(1000);
+		String lNameInForm = driver.findElement(By.id("lastname")).getAttribute("value");
+		Assert.assertEquals(firstName, lNameInForm);
+	}
+	
+	@Test 
+	public static void testRadioButton() throws InterruptedException {
+		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/java/Tutorial2/forms.html");
+		Thread.sleep(1000);
+		driver.findElement(By.id("firstname")).sendKeys("Clara");
+		driver.findElement(By.id("lastname")).sendKeys("Johnson");
+		driver.findElement(By.id("f")).click();
+		Thread.sleep(1000);
+		Boolean isFemaleSelected = driver.findElement(By.id("f")).isSelected();
+		Assert.assertTrue( isFemaleSelected);
+	}
+	
+	@Test 
+	public static void testSubmitButton() throws InterruptedException {
+		driver.get("file:///"+System.getProperty("user.dir")+"/src/main/java/Tutorial2/forms.html");
+		Thread.sleep(1000);
+		driver.findElement(By.id("firstname")).sendKeys("Clara");
+		driver.findElement(By.id("lastname")).sendKeys("Johnson");
+		driver.findElement(By.id("f")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.id("submit")).click();
+		Assert.assertEquals( "Hi "+"Clara", driver.findElement(By.id("holder")).getText());
+	}
+}
